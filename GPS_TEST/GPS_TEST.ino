597df2e2a4806f51b08098994284d11259cdbc96
e-Gizmo Mechatronix Central <egizmo.technical@gmail.com>
2017-10-17 18:42:21 +0800

    Update GPS_TEST.ino

--- GPS_TEST/GPS_TEST.ino
+++ GPS_TEST/GPS_TEST.ino
@@ -7,23 +7,16 @@
  http://www.e-gizmo.com
  */
 
-//#include <SoftwareSerial.h>
-
-//SoftwareSerial mySerial(2, 3);
 
 void setup()  
 {
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  //mySerial.begin(9600);
 }
 
 void loop() // run over and over
 {
-    //while(!(mySerial.available())){}
-    // Serial.write(mySerial.read());
-
-   while(!(Serial.available())){}
-    //char inChar = (char) Serial.read();
-Serial.write(Serial.read());
-    
+  if (Serial.available())
+    Serial.write(Serial.read());
 }
+
